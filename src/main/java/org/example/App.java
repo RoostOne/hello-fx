cbbb20023225a4625fe2bb4f31771eb078687b9e
Torsten Hahn <torsten@roost.one>
2021-04-19 13:02:28 +0200

    add gson

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,11 @@
 package org.example;
 
+import com.google.gson.Gson;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 
